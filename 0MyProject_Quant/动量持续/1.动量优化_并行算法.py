41ae598e5d461e22b950f4c63577a6f7d66b652b
MuSaCN <435116098@qq.com>
2020-10-01 14:13:10 +0800

    update

--- 0MyProject_Quant/动量持续/1.动量优化_并行算法.py
+++ 0MyProject_Quant/动量持续/1.动量优化_并行算法.py
@@ -152,9 +152,9 @@ if __name__ == '__main__':
             continue
         finish_symbol = []
         for symbol in symbol_list:
-            # if timeframe ==  "TIMEFRAME_M4" and symbol in ['EURUSD', 'GBPUSD', 'USDCHF', 'USDJPY', 'USDCAD', 'AUDUSD', 'AUDNZD', 'AUDCAD', 'AUDCHF', 'AUDJPY', 'CHFJPY', 'EURGBP', 'EURAUD', 'EURCHF', 'EURJPY', 'EURNZD']:
-            #     finish_symbol.append(symbol)
-            #     continue
+            if timeframe ==  "TIMEFRAME_M2" and symbol in ['EURUSD', 'GBPUSD', 'USDCHF', 'USDJPY', 'USDCAD', 'AUDUSD', 'AUDNZD', 'AUDCAD', 'AUDCHF', 'AUDJPY', 'CHFJPY', 'EURGBP', 'EURAUD', 'EURCHF', 'EURJPY', 'EURNZD', 'EURCAD', 'GBPCHF', 'GBPJPY', 'CADCHF', 'CADJPY', 'EURTRY', 'GBPNZD', 'USDDKK', 'USDHKD', 'USDNOK', 'USDSEK', 'USDSGD', 'USDTRY', 'GBPAUD', 'GBPCAD', 'NZDJPY', 'NZDUSD', 'XAGUSD']:
+                finish_symbol.append(symbol)
+                continue
             # 设置输出目录：one symbol + one timeframe + three direct --> one folder
             folder = __mypath__.get_desktop_path() + "\\_动量研究\\{}.{}".format(symbol, timeframe)
             # 仅做多、仅做空、多空都做，保存在一个目录下
