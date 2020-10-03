27204ba465efeb09bf31e933480d90e7cd4eafd5
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-10-03 11:44:37 +0800

    update

--- 0MyProject_Quant/时间反转策略(A大于B)/2_1.最佳参数自动选择.py
+++ 0MyProject_Quant/时间反转策略(A大于B)/2_1.最佳参数自动选择.py
@@ -45,6 +45,8 @@ myPjMT5 = MyProject.MT5_MLLearning()  # MT5机器学习项目类
 myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
 #------------------------------------------------------------
 
+
+
 '''
 # 1.根据前面输出的优化结果，自动寻找最佳参数点。
 # 2.运算后的输出内容都放在主目录下的“自动参数选择1D”文件夹，然后在里面分别建立品种目录存档结果。
@@ -59,34 +61,30 @@ warnings.filterwarnings('ignore')
 
 direct_para = ["BuyOnly", "SellOnly"]  # direct_para = ["BuyOnly", "SellOnly", "All"]
 symbol_list = myPjMT5.get_all_symbol_name().tolist()
-# timeframe_list = ["TIMEFRAME_D1","TIMEFRAME_H12","TIMEFRAME_H8","TIMEFRAME_H6",
-#                   "TIMEFRAME_H4","TIMEFRAME_H3","TIMEFRAME_H2","TIMEFRAME_H1",
-#                   "TIMEFRAME_M30","TIMEFRAME_M20","TIMEFRAME_M15","TIMEFRAME_M12",
-#                   "TIMEFRAME_M10","TIMEFRAME_M6","TIMEFRAME_M5","TIMEFRAME_M4",
-#                   "TIMEFRAME_M3","TIMEFRAME_M2","TIMEFRAME_M1"]
 timeframe_list = ["TIMEFRAME_D1","TIMEFRAME_H12","TIMEFRAME_H8","TIMEFRAME_H6",
                   "TIMEFRAME_H4","TIMEFRAME_H3","TIMEFRAME_H2","TIMEFRAME_H1",
                   "TIMEFRAME_M30","TIMEFRAME_M20","TIMEFRAME_M15","TIMEFRAME_M12",
-                  "TIMEFRAME_M10"]
+                  "TIMEFRAME_M10","TIMEFRAME_M6","TIMEFRAME_M5","TIMEFRAME_M4",
+                  "TIMEFRAME_M3","TIMEFRAME_M2","TIMEFRAME_M1"]
 
 
 #%%
 myDefault.set_backend_default("agg")
 # 仅检测 holding=1 就可以了
-para_fixed_list = [{"k":None, "holding":i, "lag_trade":1} for i in range(1,1+1)]
+para_fixed_list = [{"k":None, "holding":1, "lag_trade":1}]
 # 仅根据夏普选择就可以了. ["sharpe", "calmar_ratio", "cumRet", "maxDD"]
 y_name = ["sharpe"]
 
 #%%
-order = 30 # 极值每一边用有多少点进行比较
+order = 30 # 极值每一边用有多少点进行比较。
 finish_symbol = []
 for symbol in symbol_list:
     # 批量运算，最后合并且输出表格
     total_df = pd.DataFrame([])
     for timeframe in timeframe_list:
         for direct in direct_para:
-            folder = __mypath__.get_desktop_path() + "\\_动量研究\\{}.{}".format(symbol, timeframe)
-            filepath = folder + "\\动量_{}.xlsx".format(direct)  # 选择训练集文件
+            folder = __mypath__.get_desktop_path() + "\\_反转研究\\{}.{}".format(symbol, timeframe)
+            filepath = folder + "\\反转_{}.xlsx".format(direct)  # 选择训练集文件
             filecontent = pd.read_excel(filepath)
             for para_fixed in para_fixed_list:
                 out_df = myBTV.auto_para_1D(filepath=filepath, filecontent=filecontent, para_fixed=para_fixed, y_name=y_name, order=order, plot=True, savefig="default", batch=True)
@@ -98,8 +96,6 @@ for symbol in symbol_list:
     # 显示进度
     finish_symbol.append(symbol)
     print("自动选择最佳参数1D finished:", finish_symbol)
-
-
 
 
 
