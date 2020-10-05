5910dac763b6631ccc4c87b03e3a64b518238fa9
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-10-05 16:55:08 +0800

    update

--- 0MyProject_Quant/时间动量策略(A大于B)/2_2.自动原始策略回测.py
+++ 0MyProject_Quant/时间动量策略(A大于B)/2_2.自动原始策略回测.py
@@ -63,6 +63,10 @@ evaluate = "sharpe"
 #%%
 finish_symbol = []
 for symbol in symbol_list:
+    if symbol in ['AUDCAD', 'AUDCHF', 'AUDNZD', 'CADCHF', 'CADJPY', 'EURTRY', 'GBPNZD', 'EURNZD', 'USDDKK', 'USDHKD', 'USDNOK', 'USDSEK', 'USDSGD', 'USDTRY', 'AUDJPY', 'AUDUSD', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPUSD', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY', 'XAGUSD']:
+        finish_symbol.append(symbol)
+        continue
+    # ---
     folder_para1D = __mypath__.get_desktop_path() + "\\_动量研究\\自动参数选择1D\\%s"%symbol
     filepath_para1D = folder_para1D + "\\%s_aotu_para_1D.xlsx"%symbol
     filecontent = pd.read_excel(filepath_para1D)
