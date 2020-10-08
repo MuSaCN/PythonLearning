82354458024124b8e4637718e0a94fff091bb1ee
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-10-08 16:01:42 +0800

    update

--- 0MyProject_Quant/时间动量策略(A大于B)/2_1.最佳参数自动选择_并行.py
+++ 0MyProject_Quant/时间动量策略(A大于B)/2_1.最佳参数自动选择_并行.py
@@ -117,15 +117,27 @@ def run_auto_choose_opt(para):
 cpu_core = -1 # -1表示留1个进程不执行运算。
 # ---多进程必须要在这里执行
 if __name__ == '__main__':
-    order_list = [30,40,50]  # [30,40,50] 极值每一边用有多少点进行比较
     symbol_list = myPjMT5.get_all_symbol_name().tolist()
-    para_muilt = [(order,symbol) for order in order_list for symbol in symbol_list]
-    # ---开始多核执行
+    # ---多步并行，以更好的控制进度
+    para_muilt_30 = [(30, symbol) for symbol in symbol_list]
+    para_muilt_40 = [(40, symbol) for symbol in symbol_list]
+    para_muilt_50 = [(50, symbol) for symbol in symbol_list]
     import timeit
+    # ---开始多核执行
     t0 = timeit.default_timer()
-    myBTV.multi_processing(run_auto_choose_opt, para_muilt, core_num=cpu_core)
+    myBTV.multi_processing(run_auto_choose_opt, para_muilt_30, core_num=cpu_core)
     t1 = timeit.default_timer()
-    print("\n", '耗时为：', t1 - t0)
+    print("\n", 'para_muilt_30 耗时为：', t1 - t0)
+    # ---开始多核执行
+    t0 = timeit.default_timer()
+    myBTV.multi_processing(run_auto_choose_opt, para_muilt_40, core_num=cpu_core)
+    t1 = timeit.default_timer()
+    print("\n", 'para_muilt_40 耗时为：', t1 - t0)
+    # ---开始多核执行
+    t0 = timeit.default_timer()
+    myBTV.multi_processing(run_auto_choose_opt, para_muilt_50, core_num=cpu_core)
+    t1 = timeit.default_timer()
+    print("\n", 'para_muilt_50 耗时为：', t1 - t0)
 
 
 
