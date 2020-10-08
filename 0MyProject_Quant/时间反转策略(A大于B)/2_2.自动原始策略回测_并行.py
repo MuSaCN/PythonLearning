e7c6a321d5ae5f3945deb35a4cad3d266236c2d9
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-10-08 15:06:29 +0800

    update

--- 0MyProject_Quant/时间反转策略(A大于B)/2_2.自动原始策略回测_并行.py
+++ 0MyProject_Quant/时间反转策略(A大于B)/2_2.自动原始策略回测_并行.py
@@ -1,4 +1,7 @@
 # Author:Zhang Yuan
+import warnings
+warnings.filterwarnings('ignore')
+
 from MyPackage import *
 import numpy as np
 import pandas as pd
@@ -45,41 +48,41 @@ myPjMT5 = MyProject.MT5_MLLearning()  # MT5机器学习项目类
 myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
 #------------------------------------------------------------
 
-
-
 '''
-# 1.根据前面整理的最佳参数表格文档，读取参数，再做原始的策略测试。
-# 2.策略结果保存到“自动参数选择1D\品种\原始策略测试”文件夹下面
+# 1.根据前面整理的自动选择的最佳参数表格文档，读取参数，再做原始的策略测试。
+# 2.策略结果保存到“自动参数选择1D_**\品种\原始策略测试”文件夹下面。
+# 3.策略测试所用的区间要增大。
+# 4.回测结果较多，构成策略库供后续选择研究。
+# 5.并行运算注意内存释放。
 '''
+
 
 #%% 根据 非策略参数 定位文件 ###########################
-import warnings
-warnings.filterwarnings('ignore')
-
-symbol_list = myPjMT5.get_all_symbol_name().tolist()
 # 策略内参数(非策略参数 symbol、timeframe、direct 会自动解析)
 para_name = ["k", "holding", "lag_trade"]
 # 仅根据夏普选择就可以了.
 evaluate = "sharpe"
 
+
 #%%
-finish_symbol = []
-for symbol in symbol_list:
-    # ---文件目录 ***(修改这句)***
-    folder_para1D = __mypath__.get_desktop_path() + "\\_反转研究\\自动参数选择1D\\%s"%symbol
-    filepath_para1D = folder_para1D + "\\%s_aotu_para_1D.xlsx"%symbol
+# 自动策略测试 order = para[0]； symbol = para[1]； filter_level = para[2]；
+def run_auto_stratgy_test(para):
+    order = para[0]
+    symbol = para[1]
+    filter_level = para[2]  # 选择哪个过滤表格"filter0, filter1, filter2".
+    # ---文档定位 ***修改这里***
+    folder_para1D = __mypath__.get_desktop_path() + "\\_反转研究\\自动参数选择1D_%s\\%s"%(order, symbol)
+    filepath_para1D = folder_para1D + "\\%s_aotu_para_1D_%s.xlsx" % (symbol, filter_level)
     filecontent = pd.read_excel(filepath_para1D)
     # ---解析，显然没有内容则直接跳过
     for i in range(len(filecontent)):
-        # ---获取各参数、策略评价、参数字符串
+        # ---获取各参数和策略评价
         timeframe = filecontent.iloc[i]["timeframe"]
         direct = filecontent.iloc[i]["direct"]
-        # 策略参数
         k = filecontent.iloc[i][para_name[0]]
         holding = filecontent.iloc[i][para_name[1]]
         lag_trade = filecontent.iloc[i][para_name[2]]
-        # 训练集策略评价
-        eva_train = filecontent.iloc[i][evaluate]
+        eva_train = filecontent.iloc[i][evaluate] # 训练集策略评价
         # 解析参数生成字符串变量，用于 添加策略图的标注 和 输出图片命名。
         para_str = ""
         for name in para_name:
@@ -91,9 +94,12 @@ for symbol in symbol_list:
         # 单独测试对全数据进行测试，训练集、测试集仅画区间就可以了
         train_x0 = data_train.index[0]
         train_x1 = data_train.index[-1]
-        # ---获取信号数据 ***(修改这句)***
+        # 把训练集的时间进行左右扩展
+        bound_left, bound_right = myPjMT5.extend_train_time(train_t0=train_x0, train_t1=train_x1, extend_scale=0)
+        # 再次重新加载下全部的数据
+        data_total = myPjMT5.getsymboldata(symbol, timeframe, bound_left, bound_right, index_time=True, col_capitalize=True)
+        # ---获取信号数据 ***修改这里***
         signaldata = myBTV.stra.momentum(data_total.Close, k=k, holding=holding, sig_mode=direct, stra_mode="Reverse")
-        # ---根据交易方向获取信号
         if direct == "BuyOnly":
             signaldata_input = signaldata["buysignal"]
         elif direct == "SellOnly":
@@ -107,7 +113,7 @@ for symbol in symbol_list:
         ax.annotate(s="%s train=%.4f,all=%.4f"%(evaluate, eva_train, eva_all), xy=[train_x0, y1], xytext=[train_x0, y1])
         ax.annotate(s="%s" % para_str, xy=[train_x0, 1], xytext=[train_x0, 1])
         # ---保存输出图片
-        savefig = folder_para1D + "\\原始策略回测\\{}.{}({}).png".format(timeframe,direct,para_str)
+        savefig = folder_para1D + "\\原始策略回测_{}\\{}.{}({}).png".format(filter_level,timeframe,direct,para_str)
         import os
         os.makedirs(os.path.dirname(savefig), exist_ok=True)
         fig = ax.get_figure()
@@ -115,14 +121,26 @@ for symbol in symbol_list:
         # 关闭图片，在批量操作时，释放内存
         plt.close(fig)
         plt.show()
-        print(symbol,timeframe,direct,para_str,"完成！")
+        # print(symbol,timeframe,direct,para_str,"完成！")
     # ---显示进度
-    finish_symbol.append(symbol)
-    print("自动原始策略回测 finished:", finish_symbol)
+    print("自动原始策略回测 finished:", order, symbol, filter_level)
 
 
-
-
+#%%
+################# 多进程执行函数 ########################################
+cpu_core = -1 # -1表示留1个进程不执行运算。
+# ---多进程必须要在这里执行
+if __name__ == '__main__':
+    order_list = [30,40,50]  # [30,40,50] 极值每一边用有多少点进行比较
+    symbol_list = myPjMT5.get_all_symbol_name().tolist()
+    filter_level_list = ["filter1"] # 仅回测过滤1次的数据就可以了
+    para_muilt = [(order,symbol,filter_level) for order in order_list for symbol in symbol_list for filter_level in filter_level_list]
+    # ---开始多核执行
+    import timeit
+    t0 = timeit.default_timer()
+    myBTV.multi_processing(run_auto_stratgy_test, para_muilt, core_num=cpu_core)
+    t1 = timeit.default_timer()
+    print("\n", '耗时为：', t1 - t0)
 
 
 
