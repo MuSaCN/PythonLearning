0f3eabafe3598b71a93dcd896472ad1f5fa3fcca
MuSaCN <435116098@qq.com>
2020-10-31 09:31:03 +0800

    update

--- 0MyProject_Quant/时间反转策略(A大于B)/6_3.方向过滤策略回测_并行.py
+++ 0MyProject_Quant/时间反转策略(A大于B)/6_3.方向过滤策略回测_并行.py
@@ -157,6 +157,7 @@ if __name__ == '__main__':
                       "TIMEFRAME_M30", "TIMEFRAME_M20", "TIMEFRAME_M15", "TIMEFRAME_M12",
                       "TIMEFRAME_M10", "TIMEFRAME_M6", "TIMEFRAME_M5", "TIMEFRAME_M4",
                       "TIMEFRAME_M3", "TIMEFRAME_M2", "TIMEFRAME_M1"]
+    timeframe_list = ["TIMEFRAME_M1"]
     # 以时间框来分组
     finish_timeframe = []
     for timeframe in timeframe_list:
