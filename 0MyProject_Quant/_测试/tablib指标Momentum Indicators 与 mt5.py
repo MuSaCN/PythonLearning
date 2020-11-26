4a1b5641d29dec1f6cca84173dd36fd21c13abfe
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-11-26 16:34:56 +0800

    update

--- 0MyProject_Quant/_测试/tablib指标Momentum Indicators 与 mt5.py
+++ 0MyProject_Quant/_测试/tablib指标Momentum Indicators 与 mt5.py
@@ -48,7 +48,7 @@ myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示�
 
 #%%
 # ---获取数据
-eurusd = myMT5Pro.getsymboldata("EURUSD","TIMEFRAME_D1",[2000,1,1,0,0,0],[2020,11,24,0,0,0],index_time=True, col_capitalize=False)
+eurusd = myMT5Pro.getsymboldata("EURUSD","TIMEFRAME_D1",[2000,1,1,0,0,0],[2020,11,25,0,0,0],index_time=True, col_capitalize=False)
 
 #%%
 ### Momentum Indicators
