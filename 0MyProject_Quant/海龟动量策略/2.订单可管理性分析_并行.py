b7b669924cb3ed28f60712cf8ca2bfcdc7232d2a
MuSa <39754824+MuSaCN@users.noreply.github.com>
2021-02-25 17:16:52 +0800

    update

--- 0MyProject_Quant/海龟动量策略/2.订单可管理性分析_并行.py
+++ 0MyProject_Quant/海龟动量策略/2.订单可管理性分析_并行.py
@@ -63,88 +63,19 @@ from MyPackage.MyProjects.向量化策略测试.More_Holding import Auto_More_Ho
 more_h = Auto_More_Holding()
 
 #%% ******修改这里******
-strategy_para_name = ["n", "holding", "lag_trade"]
-symbol_list = myMT5Pro.get_main_symbol_name_list()
-total_folder = "F:\\工作---策略研究\\公开的海龟策略\\_海龟动量研究"
-label1, label2 = "sharpe", "winRate"  # 策略训练集多holding回测，选择夏普比和胜率来分析
-readfile_suffix = ".original" # 输入的文档加后缀，""表示不加，加词缀要加点号".original"
-outfile_suffix = ".holdingtest" # 输出的文档加后缀
+more_h.strategy_para_name = ["n", "holding", "lag_trade"]
+more_h.symbol_list = myMT5Pro.get_main_symbol_name_list()
+more_h.total_folder = "F:\\工作---策略研究\\公开的海龟策略\\_海龟动量研究"
+more_h.label1, more_h.label2 = "sharpe", "winRate"  # 策略训练集多holding回测
+more_h.readfile_suffix = ".original" # 输入的文档加后缀，""表示不加，加词缀要加点号".original"
+more_h.outfile_suffix = ".holdingtest" # 输出的文档加后缀
+more_h.holding_testcount = 3 # 测试到的holding数量
 
 #%% ******修改函数******
 #  策略的当期信号(不用平移)：para_list策略参数，默认-1为lag_trade，-2为holding。
 def stratgy_signal(dataframe, para_list=list or tuple):
     return myBTV.stra.turtle_momentum(dataframe, para_list[0], price_arug= ["High", "Low", "Close"])
-stratgy_signal = stratgy_signal
-
-#%%
-para = ["EURUSD"]
-symbol = para[0]  # symbol = "EURUSD"
-print("%s 开始订单可管理性分析..." % symbol)
-# ---定位策略参数自动选择文档，获取各组参数
-in_file = total_folder + "\\策略参数自动选择\\{}\\{}.total.{}{}.xlsx".format(symbol, symbol, "filter1", readfile_suffix)   # 固定只分析 filter1
-out_folder = __mypath__.dirname(in_file, 0)  # "...\\策略参数自动选择\\EURUSD"
-filecontent = pd.read_excel(in_file)
-copyfile = filecontent.copy() # 备份以便于操作
-
-#%%
-for i in range(len(filecontent)):  # i=0
-    # ---解析文档
-    # 获取各参数
-    timeframe = filecontent.iloc[i]["timeframe"]
-    direct = filecontent.iloc[i]["direct"]
-    # 策略参数
-    strat_para = [filecontent.iloc[i][strategy_para_name[j]] for j in
-                  range(len(strategy_para_name))]
-    # 满足3个标的指标都是递增才输出路径
-    suffix = myBTV.string_strat_para(strategy_para_name, strat_para)
-
-    # ---准备数据
-    date_from, date_to = myMT5Pro.get_date_range(timeframe)
-    data_total = myMT5Pro.getsymboldata(symbol, timeframe, date_from, date_to, index_time=True, col_capitalize=True)
-    data_train, data_test = myMT5Pro.get_train_test(data_total, train_scale=0.8)
-
-    # ---信号的时间可管理性分析，以3个holding是否递增来判断。
-    # 展开holding参数
-    holding_test = [holding for holding in range(1, 3 + 1)]
-    # 下面的信号质量计算是否重复持仓都要分析。重复持仓主要看胜率。
-    out_list1_NoRe, out_list2_NoRe, out_list1_Re, out_list2_Re = [], [], [], []
-
-    # ---
-    for holding in holding_test:  # holding=1
-        # 策略参数更换 -2位置的holding参数
-        para_list = strat_para[0:-2] + [holding] + [strat_para[-1]]
-        # 获取信号数据
-        signal = stratgy_signal(data_train, para_list=para_list)
-        # 信号分析，无重复持仓模式: signal_quality_NoRepeatHold / signal_quality
-        outStrat_NoRe, outSignal_NoRe = myBTV.signal_quality_NoRepeatHold(signal=signal[direct], price_DataFrame=data_train, holding=holding, lag_trade=para_list[-1], plotStrat=False)
-        out_list1_NoRe.append(outStrat_NoRe[direct][label1])
-        out_list2_NoRe.append(outStrat_NoRe[direct][label2])
-        # 信号分析，可重复持仓模式：
-        outStrat_Re, outSignal_Re = myBTV.signal_quality(signal=signal[direct], price_DataFrame=data_train, holding=holding, lag_trade=para_list[-1], plotStrat=False)
-        out_list1_Re.append(outStrat_Re[direct][label1])
-        out_list2_Re.append(outStrat_Re[direct][label2])
-    # ---判断是否单调增
-    condition1 = out_list1_NoRe[0]<=out_list1_NoRe[1] and out_list1_NoRe[1] <=out_list1_NoRe[2]
-    condition2 = out_list1_Re[0]<=out_list1_Re[1] and out_list1_Re[1]<=out_list1_Re[2]
-    # ---如果不是单调增，则备份的内容删除对应行
-    if(not (condition1 and condition2)):
-        copyfile = copyfile.drop(i)
-
-#%%
-# ---备份的内容重建索引
-copyfile.reset_index(inplace=True)
-copyfile.to_excel(out_folder + "\\{}.total.{}{}.xlsx".format(symbol, "filter1", outfile_suffix))
-
-
-
-
-
-
-
-
-
-
-
+more_h.stratgy_signal = stratgy_signal
 
 
 #%%
