5fdab2dc5533ed3ce3fef4732ac1951de3d70a67
MuSa <39754824+MuSaCN@users.noreply.github.com>
2021-03-04 09:37:16 +0800

    update

--- 0MyProject_Quant/时间反转策略(A大于B)/2.订单可管理性分析及回测_并行.py
+++ 0MyProject_Quant/时间反转策略(A大于B)/2.订单可管理性分析及回测_并行.py
@@ -72,7 +72,7 @@ more_h = Auto_More_Holding()
 
 
 #%% ******修改这里******
-more_h.strategy_para_name = ["n", "holding", "lag_trade"]
+more_h.strategy_para_name = ["k", "holding", "lag_trade"]
 more_h.symbol_list = myMT5Pro.get_main_symbol_name_list()
 more_h.total_folder = "F:\\工作---策略研究\\简单的动量反转\\_反转研究"
 more_h.readfile_suffix = ".original" # 输入的文档加后缀
