9a124df4ba3bcf540fc3417642bd4f4f7111cbbf
MuSa <39754824+MuSaCN@users.noreply.github.com>
2021-03-21 14:29:44 +0800

    update

--- 0MyProject_Quant/0.通用策略分析/2.MT5策略输出结果分析.py
+++ 0MyProject_Quant/0.通用策略分析/2.MT5策略输出结果分析.py
@@ -44,63 +44,17 @@ myTensor = MyDeepLearning.MyClass_TensorFlow()  # Tensorflow综合类
 myMT5 = MyMql.MyClass_ConnectMT5(connect=False)  # Python链接MetaTrader5客户端类
 myMT5Pro = MyMql.MyClass_ConnectMT5Pro(connect=False)  # Python链接MT5高级类
 myMT5Indi = MyMql.MyClass_MT5Indicator()  # MT5指标Python版
+myMT5Report = MyMql.MyClass_StratTestReport() # MT5策略报告类
 myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
 # ------------------------------------------------------------
 
 file = __mypath__.get_desktop_path() + "\\MT5test.xlsx"
 
-total_file = pd.read_excel(file, header=None)
-# ---获取Orders和Deals的序号
-index_order = total_file[0][total_file[0] == "Orders"].index[0]
-index_deal = total_file[0][total_file[0] == "Deals"].index[0]
-index_setting = total_file[0][total_file[0] == "Settings"].index[0]
-index_result = total_file[0][total_file[0] == "Results"].index[0]
-
-# ---整理下策略设置
-strat_setting = total_file[index_setting+1:index_result]
-strat_setting.dropna(axis=0,how='all',inplace=True)
-strat_setting.dropna(axis=1,how='all',inplace=True)
-strat_setting.reset_index(drop=True,inplace=True)
-strat_setting.columns = [i for i in range(len(strat_setting.columns))]
-
-# ---整理下策略结果
-strat_result = total_file[index_result+1:index_order]
-strat_result.dropna(axis=0,how='all',inplace=True)
-strat_result.dropna(axis=1,how='all',inplace=True)
-strat_result.reset_index(drop=True,inplace=True)
-strat_result.columns = [i for i in range(len(strat_result.columns))]
-# 是否需要转换竖排格式
-strat_result1 = pd.DataFrame([],columns=[0,1])
-for i in range(len(strat_result)): # i=0
-    for j in range(0, len(strat_result.columns)-1, 2): # j=0
-        temp = strat_result.iloc[i:i+1,j:j+2]
-        temp.columns = strat_result1.columns
-        strat_result1 = pd.concat((strat_result1,temp), axis=0, ignore_index=True)
-strat_result1.dropna(axis=0,how='all',inplace=True)
-strat_result1.reset_index(drop=True,inplace=True)
+strat_setting, strat_result, order_content, deal_content = myMT5Report.read_report_xlsx(file)
 
 
 
 
-
-
-# ---整理下Orders
-order_file = total_file[index_order:index_deal]
-order_columns = order_file.iloc[1] # 第一次的列名，包括 NaN 列
-order_content = order_file[2:]
-order_content.columns = order_columns.tolist()
-order_columns = order_columns.dropna() # 第二次的列名，丢弃NaN列
-order_content = order_content[order_columns]
-order_content.reset_index(drop=True,inplace=True)
-
-# ---整理下Deals
-deal_file = total_file[index_deal:]
-deal_columns = deal_file.iloc[1] # 第一次的列名，包括 NaN 列
-deal_content = deal_file[2:]
-deal_content.columns = deal_columns.tolist()
-deal_columns = deal_columns.dropna() # 第二次的列名，丢弃NaN列
-deal_content = deal_content[deal_columns]
-deal_content.reset_index(drop=True,inplace=True)
 
 
 
