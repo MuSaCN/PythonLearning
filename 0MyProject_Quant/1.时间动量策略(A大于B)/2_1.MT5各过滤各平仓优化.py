ed80c460a9312028a9ff5566c135bd4e6895b587
MuSa <39754824+MuSaCN@users.noreply.github.com>
2021-06-12 08:36:24 +0800

    update

--- 0MyProject_Quant/1.时间动量策略(A大于B)/2_1.MT5各过滤各平仓优化.py
+++ 0MyProject_Quant/1.时间动量策略(A大于B)/2_1.MT5各过滤各平仓优化.py
@@ -63,9 +63,9 @@ myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示�
 
 
 #%% ******参数优化******
-expertname = "My_Experts\简单动量策略(AB比较)_Test\EURUSD\EURUSD.H8.B.各过滤各平仓.ex5"
+expertname = "My_Experts\简单动量策略(AB比较)_Test\EURUSD\EURUSD.H12.B.各过滤各平仓.ex5"
 symbol = "EURUSD"
-timeframe = "TIMEFRAME_H8"
+timeframe = "TIMEFRAME_H12"
 direct = "BuyOnly"
 
 reportfolder = r"F:\工作(同步)\工作---MT5策略研究\1.简单的动量策略\{}.{}\{}".format(symbol, myMT5code.timeframe_to_affix(timeframe), direct)
