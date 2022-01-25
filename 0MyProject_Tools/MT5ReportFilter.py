eed6f19cb08d6d061977b5d172a76b003da7bfb2
MuSa <39754824+MuSaCN@users.noreply.github.com>
2022-01-25 18:02:25 +0800

    update

--- 0MyProject_Tools/MT5ReportFilter.py
+++ 0MyProject_Tools/MT5ReportFilter.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
         c_report_filter.load_report()
         # # ---并行运算，输出过滤的文本文档
         c_report_filter.main_filter_and_xlsx()
-        # # ---参数过滤自动选择，且画图、输出xlsx。
+        # ---参数过滤自动选择，且画图、输出xlsx。
         c_report_filter.main_auto_kalman_choose()
         # ---并行运算，卡尔曼选择后策略回测
         c_report_filter.main_auto_kalman_stratgy_test()
