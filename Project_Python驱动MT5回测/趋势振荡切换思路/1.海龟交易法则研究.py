757dfaad0341a0256478538e3fb459f800ef118d
MuSa <39754824+MuSaCN@users.noreply.github.com>
2022-03-11 12:05:50 +0800

    update

--- Project_Python驱动MT5回测/趋势振荡切换思路/1.海龟交易法则研究.py
+++ Project_Python驱动MT5回测/趋势振荡切换思路/1.海龟交易法则研究.py
@@ -72,12 +72,12 @@ fromdate = "2010.01.01"
 todate = "2017.01.01"
 symbol = "EURUSD"
 timeframe = "TIMEFRAME_M15"
-reportfolder = r"F:\工作(同步)\工作---MT5策略研究\海龟交易法则_趋势振荡分类研究"
-
+totalfolder = r"F:\工作(同步)\工作---MT5策略研究\海龟交易法则_趋势振荡分类研究"
+reportfolder = totalfolder + "\\{}.{}\\{}".format(symbol, timeframe, expertfile.rsplit(sep=".", maxsplit=1)[0])
 
 
 #%% ###### Step1.0 优化信号参数和固定持仓 ######
-reportfile = reportfolder + "\\{}.{}\\{}\\1.opt信号固定持仓.xml".format(symbol, timeframe, expertfile.rsplit(sep=".", maxsplit=1)[0])
+reportfile = reportfolder + "\\1.opt信号固定持仓.xml"
 optimization = 1 # 0 禁用优化, 1 "慢速完整算法", 2 "快速遗传算法", 3 "所有市场观察里选择的品种"
 # ---
 myMT5run.__init__()
@@ -106,10 +106,13 @@ myMT5run.check_inputs_and_write()
 myMT5run.run_MT5()
 
 #%% ###### Step1.1 找寻随着持仓周期增加策略表现递增的信号参数 ######
+# reportfolder
 opt = myMT5Report.read_opt_xml(reportfile)
-# ---
-# myDefault.set_backend_default("tkagg")
+
+myDefault.set_backend_default("agg") # 后台输出图片
+
 # ---信号参数，排除 "FixedHolding" 后剩下的；固定持仓；Z值
+# 参数1为信号参数，参数2为固定持仓，参数Z为策略表现
 para1,para2,paraZ = opt.columns[-2:].drop("FixedHolding")[0], "FixedHolding",  "Custom"
 X, Y, Z = opt[para1], opt[para2], opt[paraZ]
 
@@ -117,96 +120,101 @@ X, Y, Z = opt[para1], opt[para2], opt[paraZ]
 myfig.__init__(1,1,figsize=[1024,768])
 myfig.set_axes_3d2d()
 myfig.plot3Ddf_trisurf(xs=X,ys=Y,zs=Z, PlotLabel=[paraZ,para1,para2])
+myfig.savefig(reportfolder + "\\1.a.3D信号固定持仓.jpg")
 
 # ---选择固定持仓1期，且交易数量平均每天1次的
 opt1 = opt[(opt["FixedHolding"]==1) & (opt["Trades"]>=250*7)]
 opt1.set_index(keys=para1, drop=False, inplace=True)
 
-# ['Expected Payoff', 'Profit Factor', 'Recovery Factor', 'Sharpe Ratio', 'Custom']
-opt1["Custom"].plot()
-plt.show()
-opt1["Recovery Factor"].plot()
-plt.show()
+# ---分别输出各策略结果的卡尔曼过滤选择结果
+totalindex = []
+for name in ['Profit', 'Expected Payoff', 'Profit Factor', 'Recovery Factor', 'Sharpe Ratio', 'Custom']: # name = "Expected Payoff"
+    # 有时候夏普比会都为-5
+    if len(opt1[name].unique())==1:
+        continue
+    # 输出结果
+    index, values, ax = myDA.plot_kalman_and_extrema(array = opt1[name], arrayX = opt1[para1], restore_nan=False, comparator=np.greater_equal, order=30, filterlevel=1, ylabel = name,savefig=reportfolder+"\\1.a.信号1期.%s.jpg"%name, batch=True, show=True)
+    # 特殊结果占权重两个
+    indexlist = index[0].tolist() * 2 if name in ["Sharpe Ratio", "Custom"] else index[0].tolist()
+    totalindex = totalindex + indexlist
+
+# ---选择考虑权重后的众数作为信号参数1。
+signalpara1 = pd.Series(totalindex).mode()[0]
 
 
-#%%
-order = 30
-array = opt1["Custom"].values
-arrayX = opt1[para1].values
-ylabel = "label"
-filterlevel = 1
-comparator = np.greater_equal # np.greater # np.greater_equal
-
-# 卡尔曼过滤
-array_filter1 = myDA.kalman_1D(array, 1, restore_nan=False)
-array_filter2 = myDA.kalman_1D(array_filter1, 1, restore_nan=False)
-
-# ---画图
-myfig.__init__(nrows=3, ncols=6, figsize=[1920, 1080], GridSpec=["[0,:]", "[1,:]", "[2,:]"],AddFigure=True)
-index0, values0, ax0 = myDA.argrelextrema(
-    array, comparator=comparator, order=order, arrayX=arrayX, plot=True, label=ylabel, ax=myfig.axeslist[0])
-index1, values1, ax1 = myDA.argrelextrema(
-    array_filter1, comparator=comparator, order=order,arrayX=arrayX, plot=True, label=ylabel, ax=myfig.axeslist[1])
-index2, values2, ax2 = myDA.argrelextrema(
-    array_filter2, comparator=comparator, order=order, arrayX=arrayX, plot=True, label=ylabel, ax=myfig.axeslist[2])
-myfig.suptitle("{0} {1}: indi_para0的卡尔曼滤波".format("indi_name", ylabel))
-
-# ---卡尔曼过滤后的值会变动，要索引到源数据的值
-if filterlevel == 0:
-    index, values, ax = index0, values0, ax0
-    y_filter = values0
-elif filterlevel == 1:  # 注意过滤后的值会变动，要索引到源数据的值。
-    temp1 = [array[arrayX == i] for i in index1[0]]
-    index, values, ax = index1, [np.array(temp1).ravel()], ax1
-    y_filter = values1
-elif filterlevel == 2:  # 注意过滤后的值会变动，要索引到源数据的值。
-    temp2 = [array[arrayX == i] for i in index2[0]]
-    index, values, ax = index2, [np.array(temp2).ravel()], ax2
-    y_filter = values2
-
-# ---
-savefig=None
-if savefig is not None:
-    if len(index[0])>0:
-        myfig.savefig(fname=savefig)
-    else:
-        print("卡尔曼滤波自动选择没有结果：{}.{}".format("indi_name", ylabel))
-# if batch == True:
-#     myfig.close(check=False)
-#     plt.close()
-myfig.show()
-plt.show()
-
-
-
-
-
-
-#%% 单独一次回测
-reportfile = reportfolder + "\\{}.{}\\{}\\1.best_test.xml".format(symbol, timeframe, expertfile.rsplit(sep=".", maxsplit=1)[0])
+#%% ###### Step1.2 单独一次回测 ######
+reportfile = reportfolder + "\\1.b.信号={}.Fixed=1".format(signalpara1) # 不需要.xml后缀
 optimization = 0 # 0 禁用优化, 1 "慢速完整算法", 2 "快速遗传算法", 3 "所有市场观察里选择的品种"
 # ---
 myMT5run.__init__()
 myMT5run.config_Tester(expertname, symbol, timeframe, fromdate=fromdate, todate=todate,
                        delays=0, optimization=optimization, reportfile=reportfile)
 
-myMT5run.input_set("filter_mode", "1||0||0||3||Y")
-myMT5run.input_set("close_mode", "3||0||0||5||N")
-myMT5run.input_set("init_sl_FixedBars", "1900||1900||1||19000||N")
-myMT5run.input_set("holding", "12||12||1||120||N")
-myMT5run.input_set("init_sl_FixedTrailing", "0||0||1||10||N")
-myMT5run.input_set("fixed_trailing", "1950||1950||1||19500||N")
-myMT5run.input_set("init_sl_defend", "1900||1900||1||19000||N")
-myMT5run.input_set("fixed_trailing_defend", "1650||1650||1||16500||N")
-myMT5run.input_set("ATR_Period", "29||29||1||290||N")
-myMT5run.input_set("Multiple", "2.6||2.6||0.260000||26.000000||N")
-myMT5run.input_set("SAR_Step", "0.016||0.016||0.001600||0.160000||N")
-myMT5run.input_set("SAR_Max", "0.22||0.22||0.022000||2.200000||N")
-myMT5run.input_set("n_count", "38||38||1||380||N")
-myMT5run.input_set("adjust_point", "120||120||1||1200||N")
+myMT5run.input_set("Inp_ChannelPeriod", "{}||5||1||100||N".format(signalpara1))
+# ======(通用)用于分析======
+myMT5run.input_set("CustomMode", "0") # 设置自定义的回测结果 0-TB, 42-最大连亏
+myMT5run.input_set("backtestmode", "0") # 0-FitnessPerformance
+# ------1.固定持仓------
+myMT5run.input_set("FixedHolding", "1||1||1||10||N") # 0表示不是固定持仓模式，>0表示固定周期持仓。
+# ------2.信号过滤------
+myMT5run.input_set("FilterMode", "0||0||0||4||N") # 0-NoFilter, 1-Range, 2-TwoSide
+# ------3.止盈止损------
+myMT5run.input_set("InitTPPoint", "0||200||100||2000||N")
+myMT5run.input_set("SL_Init", "false") # false表示没有初期止损，true表示有初期止损。
+# ---止损设置 SL_Init=true 启用---
+myMT5run.input_set("atr_Period", "5||3||1||36||N") # 止损ATR周期.
+myMT5run.input_set("atr_N", "1.0||0.5||0.2||3.0||N") # ATR倍数.
+# ------4.重复入场------
+myMT5run.input_set("Is_ReSignal", "true") # true允许信号重复入场，false不允许信号重复入场。
+
 
 # ---检查参数输入是否匹配优化的模式，且写出配置结果。
 myMT5run.check_inputs_and_write()
-myMT5run.myini.items("TesterInputs")
 myMT5run.run_MT5()
 
+
+
+#%%
+file = reportfile + ".htm"
+
+# ---返回的结果是 DataFrame 组成的 list。
+[summary, table] = pd.read_html(file)
+
+# summary 拆分为 Settings, Results
+setting = pd.DataFrame([], columns=["name","value"])
+results = pd.DataFrame([], columns=["name","value"])
+setting = []
+results = []
+startsetting = False
+startresults = False
+for i, row in summary.iterrows():
+    row.dropna(inplace=True) # 必须丢去nan
+    rowunique = row.unique() # 元素可能会多次重复，分析唯一
+    # 跳过全部都是nan的一行
+    if len(rowunique)==1 and rowunique[0] is np.nan:
+        continue
+    # 跳过空的
+    if len(rowunique)==0:
+        continue
+    # ---
+    if rowunique[0] == "Settings":
+        startsetting = True
+    if rowunique[0] == "Results":
+        startsetting = False
+        startresults = True
+    # ---
+    if startsetting==True:
+        setting.append(rowunique)
+    if startresults==True:
+        results.append(rowunique)
+
+# ---
+pd.DataFrame(setting)
+pd.DataFrame(results)
+
+
+
+
+#%%
+
+myMT5Report.read_report_xlsx()
