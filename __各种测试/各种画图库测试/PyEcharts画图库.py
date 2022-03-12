a4bbde5caf9bf3fcdb67baa7c674f8f763ec28bf
MuSa <39754824+MuSaCN@users.noreply.github.com>
2022-03-12 17:03:15 +0800

    update

--- __各种测试/各种画图库测试/PyEcharts画图库.py
+++ __各种测试/各种画图库测试/PyEcharts画图库.py
@@ -8,12 +8,15 @@ import seaborn as sns
 import statsmodels.api as sm
 from scipy import stats
 
-#------------------------------------------------------------
+# ------------------------------------------------------------
 __mypath__ = MyPath.MyClass_Path("")  # 路径类
+mylogging = MyDefault.MyClass_Default_Logging(activate=False)  # 日志记录类，需要放在上面才行
 myfile = MyFile.MyClass_File()  # 文件操作类
 myword = MyFile.MyClass_Word()  # word生成类
 myexcel = MyFile.MyClass_Excel()  # excel生成类
+myini = MyFile.MyClass_INI()  # ini文件操作类
 mytime = MyTime.MyClass_Time()  # 时间类
+myparallel = MyTools.MyClass_ParallelCal()  # 并行运算类
 myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
 mypltpro = MyPlot.MyClass_PlotPro()  # Plot高级图系列
 myfig = MyPlot.MyClass_Figure(AddFigure=False)  # 对象式绘图类(可多个图窗)
@@ -41,12 +44,50 @@ myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
 myKeras = MyDeepLearning.MyClass_tfKeras()  # tfKeras综合类
 myTensor = MyDeepLearning.MyClass_TensorFlow()  # Tensorflow综合类
 myMT5 = MyMql.MyClass_ConnectMT5(connect=False)  # Python链接MetaTrader5客户端类
-myMT5Pro = MyMql.MyClass_ConnectMT5Pro(connect = False) # Python链接MT5高级类
+myMT5Pro = MyMql.MyClass_ConnectMT5Pro(connect=False)  # Python链接MT5高级类
+myMT5Indi = MyMql.MyClass_MT5Indicator()  # MT5指标Python版
+myMT5Report = MyMT5Report.MyClass_StratTestReport(AddFigure=False)  # MT5策略报告类
+myMT5Lots_Fix = MyMql.MyClass_Lots_FixedLever(connect=False)  # 固定杠杆仓位类
+myMT5Lots_Dy = MyMql.MyClass_Lots_DyLever(connect=False)  # 浮动杠杆仓位类
+myMT5run = MyMql.MyClass_RunningMT5()  # Python运行MT5
+myMT5code = MyMql.MyClass_CodeMql5()  # Python生成MT5代码
+myMoneyM = MyTrade.MyClass_MoneyManage()  # 资金管理类
 myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
-#------------------------------------------------------------
+# ------------------------------------------------------------
+# Jupyter Notebook 控制台显示必须加上：%matplotlib inline ，弹出窗显示必须加上：%matplotlib auto
+# %matplotlib inline
+# import warnings
+# warnings.filterwarnings('ignore')
+
+# %%
+import random
+from pyecharts import options as opts
+from pyecharts.charts import Scatter3D
+from pyecharts.faker import Faker
+
+Scatter_data = [(random.randint(0, 50), random.randint(0, 50), random.randint(0, 50)) for i in range(50)]
+scatter = (
+    Scatter3D(init_opts=opts.InitOpts(width='900px', height='600px'))  # 初始化
+        .add("", Scatter_data,
+             grid3d_opts=opts.Grid3DOpts(
+                 width=100, depth=100, rotate_speed=500, is_rotate=True
+             ))
+
+        # 设置全局配置项
+        .set_global_opts(
+        title_opts=opts.TitleOpts(title="3D散点图"),  # 添加标题
+        visualmap_opts=opts.VisualMapOpts(
+            max_=50,  # 最大值
+            pos_top=50,  # visualMap 组件离容器上侧的距离
+            range_color=Faker.visual_color  # 颜色映射
+        )
+    )
+        .render(__mypath__.get_desktop_path() + r"\3D散点图.html")
+)
 
 
-# ---获取数据
-eurusd = myMT5Pro.getsymboldata("EURUSD","TIMEFRAME_D1",[2000,1,1,0,0,0],[2020,1,1,0,0,0],index_time=True, col_capitalize=True)
-myDA.indi.candle_html(eurusd,"EURUSD",None)
+
+#%%
+
+
 
