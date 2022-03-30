f4a2244db3bedc10252f25b41eec7cf7f4bfd09d
MuSaCN <435116098@qq.com>
2022-03-30 22:43:45 +0800

    update

--- Project_Python驱动MT5回测/Pinbar裸K策略/1.Pinbar裸K策略初始回测+通用过滤.py
+++ Project_Python驱动MT5回测/Pinbar裸K策略/1.Pinbar裸K策略初始回测+通用过滤.py
@@ -76,9 +76,7 @@ timeframe_list = ["TIMEFRAME_D1","TIMEFRAME_H12","TIMEFRAME_H8","TIMEFRAME_H6",
                   "TIMEFRAME_M30","TIMEFRAME_M20","TIMEFRAME_M15","TIMEFRAME_M12",
                   "TIMEFRAME_M10","TIMEFRAME_M6","TIMEFRAME_M5","TIMEFRAME_M4",
                   "TIMEFRAME_M3","TIMEFRAME_M2","TIMEFRAME_M1"]
-timeframe_list = ["TIMEFRAME_H4","TIMEFRAME_H3","TIMEFRAME_H2","TIMEFRAME_H1",
-                  "TIMEFRAME_M30","TIMEFRAME_M20","TIMEFRAME_M15","TIMEFRAME_M12",
-                  "TIMEFRAME_M10","TIMEFRAME_M6","TIMEFRAME_M5"]
+timeframe_list = ["TIMEFRAME_M10","TIMEFRAME_M6","TIMEFRAME_M5"]
 
 
 def muiltPinbar(symbol, timeframe): # symbol=symbol_list[0] ; timeframe="TIMEFRAME_H4"
