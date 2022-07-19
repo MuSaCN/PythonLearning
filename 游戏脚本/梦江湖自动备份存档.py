4951cef221ef9d151f9a5c4ed5fde922a842bbdc
MuSa <39754824+MuSaCN@users.noreply.github.com>
2022-07-19 18:23:20 +0800

    update

---
+++ 游戏脚本/梦江湖自动备份存档.py
@@ -0,0 +1,19 @@
+# Author:Zhang Yuan
+
+
+# %% # 每隔5分钟备份一次存档
+from MyPackage import MyFile
+myfile = MyFile.MyClass_File()  # 文件操作类
+
+import time
+from datetime import datetime       #---引入datetime类(必须)
+
+while(True):
+    savefile = r"E:\games\MJH\S110000111ca0088.sav"
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    tofile = r"E:\games\MJH\存档保存\S110000111ca0088.{}.sav".format(str(now))
+    myfile.copy_dir_or_file(source=savefile,destination=tofile,DirRemove=False)
+    time.sleep(60*5) # 每隔5分钟备份一次存档
+
+
+
